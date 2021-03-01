7d9518f1527d7474d77408973d9a2f6d0380217e
Freddy Daniel Rojas <danielrojasvalda@gmail.com>
2021-03-01 14:10:56 -0500

    Project Test VTech v2 - Add comments - add message for amount - delete no use code

--- js/scripts.js
+++ js/scripts.js
@@ -1,47 +1,53 @@
-	function functionHeader() {
-		var x = document.getElementById("topnav");
-		if (x.className === "topnav") {
-			x.className += " responsive";
-		  } else {
-			x.className = "topnav";
-		  }
+/**
+ * Virgina Tech Test
+ * Freddy Rojas
+ * 02/08/2021
+ * 
+ * Funtional Scripts for index.html
+ */
+
+/*--FUNCTION HEADER--*/
+function functionHeader() {
+	var x = document.getElementById("topnav");
+	if (x.className === "topnav") {
+		x.className += " responsive";
+	} else {
+		x.className = "topnav";
 	}
+}
 
-    $(document).ready(function() {
+/*-- FUNCTION AMOUNT GROUP--*/
+$(document).ready(function() {
+	$('#amountGroup button').on('click', function() {
+    	var thisBtn = $(this);
+    	thisBtn.addClass('active').siblings().removeClass('active');
+    	var btnValue = thisBtn.val();
+  		document.getElementById("amount").value = btnValue;
+		});
+  	});
 
-  // Get click event, assign button to var, and get values from that var
-  $('#myDIV button').on('click', function() {
-    var thisBtn = $(this);
-    thisBtn.addClass('active').siblings().removeClass('active');
-//    var btnText = thisBtn.text();
-    var btnValue = thisBtn.val();
-//    console.log(btnText + ' - ' + btnValue);
-  	document.getElementById("amount").value = btnValue;
+/*-- FUNCTION AMOUNT GROUP--*/
+function maxLengthCheck(object) {
+	if (object.value.length > object.max.length) {	
+		object.value = object.value.slice(0, object.max.length);
+		alert("The minimum amount is $0.01\nThe maximum amount is $10000\nPlease check the input value: $"+object.value);
+	}
+}
 
-  });
-  
+
+
+/*-- FUNCTION FRECUENCY--*/
+$(document).ready(function(){
+	$('#frecuency').change(function(){
+		selected_value = $("input[name='frecuency_options']:checked").val();
+	});
 });
 
-
-			
-	$(document).ready(function(){
-	
-		$('#frecuency').change(function(){
-			selected_value = $("input[name='frecuency_options']:checked").val();
-		});
-	
-	});
-
-
-
-
-	function myFunction2() {
-	  document.getElementById("formGif").submit();
-	  var amount = document.getElementById('amount').value;
-	  var frecuency = $('#frecuency input:radio:checked').val()
-
-
-//var frecuency = document.getElementById('frecuency').value;
+/*-- FUNCTION SUBMIT--*/
+function functionSubmit() {
+	document.getElementById("formGif").submit();
+	var amount = document.getElementById('amount').value;
+	var frecuency = $('#frecuency input:radio:checked').val()
 	console.log('SUBMIT');
 	console.log('?amount=' +amount+ '&frecuency=' + frecuency);
-	}
+}
